c5a020e30ae54dfd07732a1c93df89814c1a7687
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-20 21:14:06 -0400

    use array arguments when calling function

--- app/functions.js
+++ app/functions.js
@@ -2,7 +2,7 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return (fn(arr[0], arr[1], arr[2]));
   },
 
   speak : function(fn, obj) {
