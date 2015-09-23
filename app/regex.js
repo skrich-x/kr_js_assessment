1aaccab615e15371718de298053e626750be963d
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-22 20:40:11 -0400

    Regular Expression to locate numbers in string

--- app/regex.js
+++ app/regex.js
@@ -1,8 +1,9 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
+  //regex to locate number in string
   containsNumber : function(str) {
-
+    return(/\d/).test(str);
   },
 
   containsRepeatingLetter : function(str) {
