33e3de9d1424d063255c5f242bee4626f39a4b7a
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-28 22:49:23 -0400

    convert to Binary

--- app/numbers.js
+++ app/numbers.js
@@ -9,7 +9,13 @@ exports.numbersAnswers = {
   },
 
   convertToBinary: function(num) {
+    var arr = [];
 
+    for (var i = 7; i > -1; i--) {
+      arr.push( num & (1 << i) ? 1 : 0 );
+    }
+
+    return arr.join('');
   },
 
   multiply: function(a, b) {
