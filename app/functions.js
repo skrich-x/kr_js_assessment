ddd917bd3e3602da846c9c9560b3d1f35b069cba
Kyle Richardson <s.k.richardson7@gmail.com>
2015-10-16 20:50:43 -0400

    implement parital function for variable # arguments

--- app/functions.js
+++ app/functions.js
@@ -52,7 +52,12 @@ exports.functionsAnswers = {
     fn.apply(null, x);
   },
 
-  partialUsingArguments : function(fn) {
+    partialUsingArguments : function(fn) {
+    var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+    return function() {
+      var x = args.concat(Array.prototype.slice.call(arguments));
+      return fn.apply(null, x);
+    };
 
   },
 
