24dd8cc9e655bbcab43909d4511065c9264aff40
Kyle Richardson <s.k.richardson7@gmail.com>
2015-10-17 19:43:36 -0400

    implement multiply function practice

--- app/numbers.js
+++ app/numbers.js
@@ -19,6 +19,24 @@ exports.numbersAnswers = {
   },
 
   multiply: function(a, b) {
+   a = adjust(a);
+   b = adjust(b);
 
-  }
+   var result = (a.adjusted * b.adjusted) / (a.multiplier * b.multiplier);
+
+   return result;
+
+   function adjust(num) {
+     var exponent, multiplier;
+
+     if (num < 1) {
+       exponent = Math.floor( Math.log(num) * -1 );
+       multiplier = Math.pow(10, exponent);
+
+       return { adjusted: num * multiplier, multiplier: multiplier };
+     }
+
+     return { adjusted: num, multiplier: 1 };
+   }
+ }
 };
